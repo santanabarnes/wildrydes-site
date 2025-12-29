c369d5e53de391921f820d299db0cded98198630
santanabarnes <158191038+santanabarnes@users.noreply.github.com>
2025-12-29 16:13:42 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_Bwe2t6Pxk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2o2fr8romalvc0r531t306kj18', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_swqUJxPNJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5d2nibpp32l6gumhqttr29m23m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
